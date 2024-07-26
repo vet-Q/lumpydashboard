667df7d22f8bf697cef09951d13a1827f5abe9cf
vetq <kwonok5945@gmail.com>
2024-07-26 17:39:28 +0100

    change index.css

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,6 +1,7 @@
-import { Box, IconButton, InputBase, useTheme } from "@mui/material";
+import { Box, IconButton, useTheme } from "@mui/material";
 import { useContext } from "react";
 import { ColorModeContext, tokens } from "../../theme";
+import InputBase from "@mui/material/InputBase";
 import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
@@ -8,28 +9,28 @@ import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 import SearchIcon from "@mui/icons-material/Search";
 
-const Topbar = () =>{
+const Topbar = () => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
     const colorMode = useContext(ColorModeContext);
 
-    return  (
-
+    return (
         <Box display="flex" justifyContent="space-between" p={2}>
             {/* SEARCH BAR */}
             <Box
                 display="flex"
                 backgroundColor={colors.primary[400]}
                 borderRadius="3px"
+                p={1}
             >
-            <InputBase sx={{ml:2 , flex: 1}} placeholder="search"/>
-            <IconButton type="button" sx={{ p: 1 }}>
-                        <SearchIcon />
-            </IconButton>
+                <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+                <IconButton type="button" sx={{ p: 1 }}>
+                    <SearchIcon />
+                </IconButton>
             </Box>
 
             {/* ICONS */}
-            <Box display="flex">
+            <Box display="flex" p={1}>
                 <IconButton onClick={colorMode.toggleColorMode}>
                     {theme.palette.mode === "dark" ? (
                         <DarkModeOutlinedIcon />
@@ -47,7 +48,7 @@ const Topbar = () =>{
                     <PersonOutlinedIcon />
                 </IconButton>
             </Box>
-    </Box>
+        </Box>
     );
 };
 
