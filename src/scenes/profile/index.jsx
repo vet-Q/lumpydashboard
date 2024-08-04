c20ebc6746f55b0e5efb1602969e4151a8a0e988
vetq <kwonok5945@gmail.com>
2024-08-05 00:44:15 +0100

    profile add

---
+++ src/scenes/profile/index.jsx
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material";
+import ProfileChart from "../../components/profileBox";
+
+const Profile = () =>{
+    return (
+        <Box m="20px">
+            <Box height="75vh">
+                <ProfileChart/>
+            </Box>
+        </Box>
+    )
+}
+
+export default Profile;
