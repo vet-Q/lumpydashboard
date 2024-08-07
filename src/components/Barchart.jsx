72bc2f22c6e4fec6066a9f8259a590c790eed50c
vetq <kwonok5945@gmail.com>
2024-08-07 01:33:13 +0100

    menubar modi

--- src/components/Barchart.jsx
+++ src/components/Barchart.jsx
@@ -1,7 +1,7 @@
 import { useTheme } from "@mui/material";
 import { ResponsiveBar } from "@nivo/bar";
 import { tokens } from "../theme";
-import { mockBarData as data } from "../data/mockData";
+import { preparedData as data } from "../data/preparedData";
 
 const BarChart = ({ isDashboard = false }) => {
     const theme = useTheme();
@@ -9,9 +9,8 @@ const BarChart = ({ isDashboard = false }) => {
 
     return (
         <ResponsiveBar
-            data={data}
+            data={data.barChartData}
             theme={{
-                // added
                 axis: {
                     domain: {
                         line: {
@@ -39,33 +38,13 @@ const BarChart = ({ isDashboard = false }) => {
                     },
                 },
             }}
-            keys={["hot dog", "burger", "sandwich", "kebab", "fries", "donut"]}
-            indexBy="country"
+            keys={["Africa", "Asia", "Europe"]}
+            indexBy="year"
             margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
             padding={0.3}
             valueScale={{ type: "linear" }}
             indexScale={{ type: "band", round: true }}
             colors={{ scheme: "nivo" }}
-            defs={[
-                {
-                    id: "dots",
-                    type: "patternDots",
-                    background: "inherit",
-                    color: "#38bcb2",
-                    size: 4,
-                    padding: 1,
-                    stagger: true,
-                },
-                {
-                    id: "lines",
-                    type: "patternLines",
-                    background: "inherit",
-                    color: "#eed312",
-                    rotation: -45,
-                    lineWidth: 6,
-                    spacing: 10,
-                },
-            ]}
             borderColor={{
                 from: "color",
                 modifiers: [["darker", "1.6"]],
@@ -76,7 +55,7 @@ const BarChart = ({ isDashboard = false }) => {
                 tickSize: 5,
                 tickPadding: 5,
                 tickRotation: 0,
-                legend: isDashboard ? undefined : "country", // changed
+                legend: isDashboard ? undefined : "Year",
                 legendPosition: "middle",
                 legendOffset: 32,
             }}
@@ -84,7 +63,7 @@ const BarChart = ({ isDashboard = false }) => {
                 tickSize: 5,
                 tickPadding: 5,
                 tickRotation: 0,
-                legend: isDashboard ? undefined : "food", // changed
+                legend: isDashboard ? undefined : "Cases",
                 legendPosition: "middle",
                 legendOffset: -40,
             }}
@@ -121,7 +100,7 @@ const BarChart = ({ isDashboard = false }) => {
             ]}
             role="application"
             barAriaLabel={function (e) {
-                return e.id + ": " + e.formattedValue + " in country: " + e.indexValue;
+                return e.id + ": " + e.formattedValue + " in year: " + e.indexValue;
             }}
         />
     );
